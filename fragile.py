b83f863eae7b0729bac3f141145ff65b8e8cdb6c
Nicholas Bollweg (Nick) <nick.bollweg@gmail.com>
2012-12-24 17:47:48 -0500

    fixed up the debug flag... need a better way... file?

--- fragile.py
+++ fragile.py
@@ -18,7 +18,7 @@ from webassets.filter.cssrewrite import CSSRewrite
 
 def make_app(env="dev"):
     
-    DEBUG = True#env == "dev"
+    DEBUG = env == "dev"
     
     url_root = {
         "dev": "/",
@@ -56,7 +56,7 @@ def make_app(env="dev"):
     app.config['CSRF_ENABLED'] = DEBUG
     app.config['SECRET_KEY'] = "totally-insecure"
     app.config['DEBUG'] = DEBUG
-    app.config['ASSETS_DEBUG'] = False
+    app.config['ASSETS_DEBUG'] = DEBUG
     app.config['BOOTSTRAP_JQUERY_VERSION'] = None
 
     Bootstrap(app)
