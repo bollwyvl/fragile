d5dc7a895d7eb04846d7f87df3d73346e8aae03e
Nicholas Bollweg (Nick) <nick.bollweg@gmail.com>
2012-12-24 22:45:14 -0500

    linting, pep8, as api.heroku.com is unreachable

--- fabfile.py
+++ fabfile.py
@@ -10,16 +10,23 @@ from fabric.api import task
 
 import sh
 
+sys.path.append(os.path.abspath(os.path.dirname(__file__)))
+
+from fragile import make_app
+
 PROJECT_ROOT = str(sh.git("rev-parse", **{"show-toplevel": True})).strip()
 PROJECT_SHA = str(sh.git("rev-parse", "HEAD")).strip()
 
+
 @task
 def proj():
+    """switch to the project directory... still doesn't work in sobmodules"""
     sh.cd(PROJECT_ROOT)
 
 
 @task
 def flake():
+    """run some flakes"""
     print(". running PyFlakes on test equipment")
     proj()
     sh.pyflakes("setup.py", "fabfile.py", "fragile.py")
@@ -27,11 +34,13 @@ def flake():
 
 @task
 def pep8():
+    """check python style"""
     sh.pep8("fabfile.py", "fragile.py")
 
 
 @task
 def build():
+    """do the build for static files hosting... mostly webassets now"""
     proj()
     flake()
     clean()
@@ -47,6 +56,7 @@ def build():
 
 @task
 def dev():
+    """set up repository for development"""
     proj()
     sh.git("submodule", "init")
     sh.git("submodule", "update")
@@ -54,24 +64,33 @@ def dev():
 
 @task
 def clean():
+    """clean up generated files"""
     proj()
     print ". cleaning up build and dist"
     try:
-        sh.rm("-r", sh.glob("dist/*"), sh.glob("build/*"))
-    except:
-        print ".. already clean"
+        sh.rm("-r",
+            sh.glob("dist/*"),
+            sh.glob("build/*")
+        )
+    except Exception, err:
+        print ".. already clean: %s" % err
 
 
 @task
 def dirty():
+    """list of uncommited changes, as this is hard to get out of submodules"""
     proj()
     dirty_file = open(".dirty", "w")
-    dirty_file.write("".join([d for d in sh.git("status", "--porcelain")
-        if d.strip().split(" ")[-1] not in [u"dist"]]))
+    dirty_file.write("".join([
+        d for d in sh.git("status", "--porcelain")
+        if d.strip().split(" ")[-1] not in [u"dist"]
+    ]))
     dirty_file.close()
+
 
 @task
 def favicon():
+    """generate the favicon... ugly"""
     proj()
     print(". generating favicons...")
     sizes = [16, 32, 64, 128]
@@ -87,103 +106,119 @@ def favicon():
             tmp_file(size))
 
     print(".. generating bundle")
+
     sh.convert(
         *[tmp_file(size) for size in sizes] + [
             "-colors", 256,
             "static/img/favicon.ico"
         ]
     )
-    
+
     print(".. cleaning up")
     sh.rm(sh.glob("/tmp/favicon-*.png"))
 
+
 @task
 def copy_assets():
+    """copy assets for static serving"""
     proj()
-    
+
     print(". copying assets ...")
+
     copy_patterns = {
-        "dist": sh.glob("./static/config/*.json") + sh.glob("./static/fragile-min.*") + ["./static/lib/jquery-1.8.3.min.js"],
+        "dist": ["./static/lib/jquery-1.8.3.min.js"] +
+        sh.glob("./static/config/*.json") +
+        sh.glob("./static/fragile-min.*"),
+
         "dist/font": sh.glob("./static/lib/awesome/font/*"),
         "dist/svg": sh.glob("./static/svg/*.svg"),
         "dist/img": sh.glob("./static/img/*.*") or [],
     }
 
     for dst, copy_files in copy_patterns.items():
-        os.path.exists(dst) or sh.mkdir("-p", dst)
+        if not os.path.exists(dst):
+            sh.mkdir("-p", dst)
+
         for c_file in copy_files:
             print "... copying", c_file, dst
             sh.cp("-r", c_file, dst)
-    
+
     wa_cache = "./dist/.webassets-cache"
-    os.path.exists(wa_cache) and sh.rm("-r", wa_cache)
+
+    if os.path.exists(wa_cache):
+        sh.rm("-r", wa_cache)
+
+
 @task
 def patch_css():
-  proj()
-  css_path = "./dist/fragile-min.css"
-    
-  css_file = open(css_path, "r")
-  css = css_file.read()
-  css_file.close()
-    
-  css_file = open(css_path, "w")
-  css_file.write(css.replace("../lib/awesome/font/", "./font/"))
-  css_file.close()
-    
+    """fix css paths... might be able to do this with `cssrewrite`"""
+    proj()
+    css_path = "./dist/fragile-min.css"
+
+    css_file = open(css_path, "r")
+    css = css_file.read()
+    css_file.close()
+
+    css_file = open(css_path, "w")
+    css_file.write(css.replace("../lib/awesome/font/", "./font/"))
+    css_file.close()
+
+
 @task
 def html():
+    """use the `build` app style to generate the hosted static html"""
     proj()
     print ". generating production html"
-    
-    fragile_path = os.path.abspath(os.path.dirname(__file__))
-    sys.path.append(fragile_path)
 
-    from fragile import make_app
     app = make_app("build")
-    
+
     prod_files = {
         "dist/index.html": "/dist/",
     }
-    
+
     for prod_file, url in prod_files.items():
         print ".. writing ", prod_file
         open(prod_file, "w").write(
             app.test_client().get(url).data
-                .replace('src="/lib/', 'src="./')
-                .replace('src="/', 'src="./')
-                .replace('href="/', 'href="./')
+            .replace('src="/lib/', 'src="./')
+            .replace('src="/', 'src="./')
+            .replace('href="/', 'href="./')
         )
 
-    
+
 @task
 def serve_dev():
+    """yup"""
     serve("dev")
+
 
 @task
 def serve_prod():
+    """also, this"""
     serve("prod")
+
 
 @task
 def serve_build():
+    """needed for building assets"""
     serve("build")
-    
+
+
 @task
 def serve_test():
+    """TODO: write some tests"""
     serve("test")
 
-    
-def serve(env):
-    fragile_path = os.path.abspath(os.path.dirname(__file__))
-    sys.path.append(fragile_path)
 
-    from fragile import make_app
+def serve(env):
+    """serve stuff... prod is mostly done by just doing `python fragile.py`"""
     app = make_app(env)
-    
+
     port = {
         "dev": 5000,
         "prod": 5001,
         "test": 5002,
         "build": 5003,
     }[env]
-    
+
     app.run(port=port)
--- fragile.py
+++ fragile.py
@@ -3,90 +3,107 @@ import os
 from os.path import join as opj
 from os.path import abspath as opa
 
+import mimetypes
+
+mimetypes.add_type("application/font-woff", ".woff")
+
 from flask import (
     Flask,
-    )
+)
 from flask import (
     render_template,
-    )
+)
 
 from flask.ext.bootstrap import Bootstrap
 
-import webassets.loaders
 from flask.ext.assets import Environment
 from webassets.filter.cssrewrite import CSSRewrite
+from webassets.loaders import YAMLLoader
+
 
 def make_app(env="dev"):
-    
-    DEBUG = env == "dev"
-    
-    url_root = {
-        "dev": "/",
-        "build": "/dist/",
-        "test": "/dist/",
-        "prod": "/"
-    }[env]
-    
+    """
+    This is still needlessly complicated.
+
+    Returns a Flask WSGI instance.
+    """
+    debug = env == "dev"
+
+    url_root = dict(
+        dev="/",
+        build="/dist/",
+        test="/dist/",
+        prod="/"
+    )[env]
+
     app_home = os.path.dirname(__file__)
-    
-    cfg = {
-        "dev": dict(
+
+    cfg = dict(
+        dev=dict(
             static_url_path="/static",
             template_folder="./templates",
             static_folder=opa(opj(app_home, "static"))
-            ),
-        "build": dict(
+        ),
+        build=dict(
             static_url_path="/",
             template_folder="./templates",
             static_folder=opa(opj(app_home, "static"))
-            ),
-        "test": dict(
+        ),
+        test=dict(
             static_url_path="/dist",
             template_folder="../dist",
             static_folder=opa(opj(app_home, "..", "dist"))
-            ),
-        "prod": dict(
+        ),
+        prod=dict(
             static_url_path="/static",
             template_folder="./templates",
             static_folder=opa(opj(app_home, "static"))
-            )
-    }[env]
-    
+        )
+    )[env]
+
     app = Flask(__name__, **cfg)
-    app.config['CSRF_ENABLED'] = DEBUG
-    app.config['SECRET_KEY'] = "totally-insecure"
-    app.config['DEBUG'] = DEBUG
-    app.config['ASSETS_DEBUG'] = DEBUG
-    app.config['BOOTSTRAP_JQUERY_VERSION'] = None
+
+    app.config.update(dict(
+        CSRF_ENABLED=debug,
+        SECRET_KEY=os.environ.get("FLASK_SECRET_KEY", "totally-insecure"),
+        DEBUG=debug,
+        ASSETS_DEBUG=debug,
+        BOOTSTRAP_JQUERY_VERSION=None
+    ))
 
     Bootstrap(app)
-    
+
     def font_stuff(url):
-      repl = "./lib/awesome/font/"
-      if env == "build":
-          repl = "./font/"
-      return url.replace("../font/", repl)
-      
-    
+        """
+        Some font URL rewriting
+        """
+        repl = "./lib/awesome/font/"
+        if env == "build":
+            repl = "./font/"
+        return url.replace("../font/", repl)
+
     fix_font_css = CSSRewrite(replace=font_stuff)
-    
+
     assets = Environment(app)
-    bundles = webassets.loaders.YAMLLoader('assets.yaml').load_bundles()
+
+    bundles = YAMLLoader('assets.yaml').load_bundles()
+
     for to_fix in ["prod", "build"]:
         bundles["css-%s" % to_fix].filters.insert(0, fix_font_css)
-    [assets.register(name, bundle) for name, bundle in bundles.iteritems()]
+
+    for name, bundle in bundles.iteritems():
+        assets.register(name, bundle)
 
     @app.route(url_root)
-    def home():
+    def index():
         kwargs = {}
-        
+
         return render_template("index.html", env=env, **kwargs)
-        
+
     return app
 
 if __name__ == '__main__':
     # this is the production version: fabric handles others
     # Bind to PORT if defined, otherwise default to 5001.
-    app = make_app("prod")
     port = int(os.environ.get("PORT", 5001))
-    app.run(host="0.0.0.0", port=port)
+    make_app("prod").run(host="0.0.0.0", port=port)
