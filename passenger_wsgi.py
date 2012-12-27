df618512bcdeeb7587c26a9a3bccd658bd39f45c
Nicholas Bollweg (Nick) <nick.bollweg@gmail.com>
2012-12-26 19:57:03 -0600

    adding passenger serving stuff

---
+++ passenger_wsgi.py
@@ -0,0 +1,15 @@
+import os
+import sys
+
+# Passenger stuff
+INTERP = os.path.join(os.environ["HOME"], "fragl_env", "bin", "python")
+
+if sys.executable != INTERP:
+    os.execl(INTERP, INTERP, *sys.argv)
+
+sys.path.append(os.getcwd())
+
+# primary import
+from fragile import make_app
+
+application = make_app("prod")
